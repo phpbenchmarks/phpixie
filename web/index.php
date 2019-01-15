814449fb70482b5f4f85389946c4b5891313b37d
Steevan BARBOYON <steevan.barboyon@gmail.com>
2019-01-15 12:58:33 +0000

    Prepare Hello world benchmark

--- web/index.php
+++ web/index.php
@@ -5,3 +5,5 @@ require_once(__DIR__.'/../vendor/autoload.php');
 $framework = new Project\Framework();
 $framework->registerDebugHandlers();
 $framework->processHttpSapiRequest();
+
+// require phpbenchmarks stats.php here when needed
